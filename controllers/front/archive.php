294644c0f884d59c54a543ff50547eff33991224
kamrulsds <kamrul@smartdatasoft.com>
2014-08-21 14:00:55 +0600

    force ssl problem solved

--- controllers/front/archive.php
+++ controllers/front/archive.php
@@ -3,7 +3,7 @@
 include_once(dirname(__FILE__).'/../../classes/controllers/FrontController.php');
 class smartblogarchiveModuleFrontController extends smartblogModuleFrontController
 {
-	public $ssl = true;
+	public $ssl = false;
 	public function init(){
             parent::init();
 	}  
