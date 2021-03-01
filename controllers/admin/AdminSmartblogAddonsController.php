82dd9b8f4bcdaa07b802f1b6240a5e8bc8a37e22
enam_office <enamhassan96@gmail.com>
2021-03-01 12:46:43 +0600

    Generating Update Link and can get version info without purchase code

--- controllers/admin/AdminSmartblogAddonsController.php
+++ controllers/admin/AdminSmartblogAddonsController.php
@@ -17,11 +17,12 @@ class AdminSmartblogAddonsController extends ModuleAdminController
         parent::__construct();
     }
 
-    public function setMedia($isNewTheme = false){
-       
+    public function setMedia($isNewTheme = false)
+    {
+
         parent::setMedia($isNewTheme);
         $this->addJS(_MODULE_SMARTBLOG_JS_DIR_ . 'addons.js');
-        Media::addJsDef( array( 'sblogaddons_ajaxurl' => $this->context->link->getAdminLink( 'AdminSmartblogAddons' ), 'controller_name' => "AdminSmartblogAddons" ) );
+        Media::addJsDef(array('sblogaddons_ajaxurl' => $this->context->link->getAdminLink('AdminSmartblogAddons'), 'controller_name' => "AdminSmartblogAddons"));
         $this->addCSS(__PS_BASE_URI__ . $this->admin_webpath . '/themes/new-theme/public/theme.css', 'all', 99999);
     }
 
@@ -130,22 +131,22 @@ class AdminSmartblogAddonsController extends ModuleAdminController
             ),
         );
 
-        foreach($addons_arr as $name => $addon){
+        foreach ($addons_arr as $name => $addon) {
 
-            if(file_exists(_PS_MODULE_DIR_ . $name)){
-                if (Module::isInstalled( $name ) &&Module::isEnabled( $name ) ) {
+            if (file_exists(_PS_MODULE_DIR_ . $name)) {
+                if (Module::isInstalled($name) && Module::isEnabled($name)) {
                     $addons_arr[$name]['installed'] = 1;
-                }else{
+                } else {
                     $addons_arr[$name]['installed'] = 0;
                 }
-            }else{
+            } else {
                 $addons_arr[$name]['installed'] = -1;
             }
         }
 
-        $template_file = _PS_MODULE_DIR_.$this->module->name.'/views/templates/admin/form.tpl';
+        $template_file = _PS_MODULE_DIR_ . $this->module->name . '/views/templates/admin/form.tpl';
 
-        $validity = Configuration::get( 'SMARTBLOG_LICENSE_VALIDITY' );
+        $validity = Configuration::get('SMARTBLOG_LICENSE_VALIDITY');
 
         $this->context->smarty->assign(array(
             'addons' =>  $addons_arr,
@@ -157,66 +158,84 @@ class AdminSmartblogAddonsController extends ModuleAdminController
         $this->context->smarty->assign(array(
             'content' =>  $content,
         ));
-        
     }
 
-    public function ajaxProcessActionAddon(){
+    public function ajaxProcessActionAddon()
+    {
 
         $module = Tools::getValue("addon");
         $act = Tools::getValue("installed");
-        if($act){
-            if ( Module::isInstalled( $module ) && Module::isEnabled( $module ) ) {
-                $mod_ins = Module::getInstanceByName( trim($module) );
+        if ($act) {
+            if (Module::isInstalled($module) && Module::isEnabled($module)) {
+                $mod_ins = Module::getInstanceByName(trim($module));
                 $mod_ins->uninstall();
                 echo '0';
             }
-        }else{
-            if ( !Module::isInstalled( $module ) ) {
-                $mod_ins = Module::getInstanceByName( trim($module) );
+        } else {
+            if (!Module::isInstalled($module)) {
+                $mod_ins = Module::getInstanceByName(trim($module));
                 $mod_ins->install();
                 echo '1';
             }
         }
         die();
     }
-
     /**
-	 * AjaxProcessDownNow processes downloading the update.
-	 *
-	 * @return void
-	 */
-	public function ajaxProcessDownNow() {
-		$down_url = Tools::getValue( 'down_url' );
+     * AjaxProcessDownNow processes downloading the update.
+     *
+     * @return void
+     */
+    public function ajaxProcessDownNow()
+    {
+        $down_url = Tools::getValue('down_url');
 
-		$down_path = _PS_MODULE_DIR_;
-		$newfile   = $down_path . '/classy_productextratab.zip';
+        if ($down_url == '') {
+            include_once _MODULE_SMARTBLOG_CLASS_DIR_ . 'SmartBlogLicense.php';
+            $licence_obj = new SmartBlogLicense(0);
+            $purchase_code = Configuration::get('SMARTBLOG_LICENSE');
+            $d_link = $licence_obj->smartblog_get_update($purchase_code, 0);
 
-		$ua = 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US) AppleWebKit/525.13 (KHTML, like Gecko) Chrome/0.A.B.C Safari/525.13';
-		$ch = curl_init();
-		curl_setopt( $ch, CURLOPT_URL, $down_url );
-		curl_setopt( $ch, CURLOPT_HEADER, false );
-		curl_setopt( $ch, CURLOPT_RETURNTRANSFER, true );
-		curl_setopt( $ch, CURLOPT_USERAGENT, $ua );
-		curl_setopt( $ch, CURLOPT_AUTOREFERER, true );
-		curl_setopt( $ch, CURLOPT_FOLLOWLOCATION, true );
-		curl_setopt( $ch, CURLOPT_MAXREDIRS, 20 );
-		$result = curl_exec( $ch );
-		file_put_contents( $newfile, $result );
-		$last = curl_getinfo( $ch, CURLINFO_EFFECTIVE_URL );
-		if ( curl_errno( $ch ) ) {
-			echo 'Could not update the module. Please try again.';
-		} else {
-			$zip = new \ZipArchive();
-			if ( $zip->open( $newfile ) === true ) {
-				$zip->extractTo( _PS_MODULE_DIR_ );
-				$zip->close();
-			}
-			Configuration::updateValue( 'SMARTBLOG_STABLE', '' );
-			Configuration::updateValue( 'SMARTBLOG_DLINK', '' );
-			echo 'Updated';
-		}
-		curl_close( $ch );
-		die();
-	}
-    
+            $return_arr = array();
+            $return_arr['status'] = 2;
+            $return_arr['msg'] = $d_link;
+            echo json_encode($return_arr);
+            die();
+        } else {
+            $down_path = _PS_MODULE_DIR_;
+            $newfile   = $down_path . '/classy_productextratab.zip';
+
+            $ua = 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US) AppleWebKit/525.13 (KHTML, like Gecko) Chrome/0.A.B.C Safari/525.13';
+            $ch = curl_init();
+            curl_setopt($ch, CURLOPT_URL, $down_url);
+            curl_setopt($ch, CURLOPT_HEADER, false);
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+            curl_setopt($ch, CURLOPT_USERAGENT, $ua);
+            curl_setopt($ch, CURLOPT_AUTOREFERER, true);
+            curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+            curl_setopt($ch, CURLOPT_MAXREDIRS, 20);
+            $result = curl_exec($ch);
+            file_put_contents($newfile, $result);
+            $last = curl_getinfo($ch, CURLINFO_EFFECTIVE_URL);
+            if (curl_errno($ch)) {
+                $return_arr = array();
+                $return_arr['status'] = 0;
+                $return_arr['msg'] = 'Could not update the module. Please try again.';
+                echo json_encode($return_arr);
+            } else {
+                $zip = new \ZipArchive();
+                if ($zip->open($newfile) === true) {
+                    $zip->extractTo(_PS_MODULE_DIR_);
+                    $zip->close();
+                }
+                Configuration::updateValue('SMARTBLOG_STABLE', '');
+                Configuration::updateValue('SMARTBLOG_DLINK', '');
+                $return_arr = array();
+                $return_arr['status'] = 1;
+                $return_arr['msg'] = 'Updated';
+                echo json_encode($return_arr);
+            }
+            curl_close($ch);
+            die();
+        }
+    }
 }
